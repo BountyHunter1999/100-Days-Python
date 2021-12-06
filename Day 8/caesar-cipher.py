b0e4e053c87014c258b1107e9343cf853e7fd9ac
mikeyy <adhikari.shrish1@gmail.com>
2021-12-06 14:58:06 +0545

    Improved the code with fewer conditions to check

--- Day 8/caesar-cipher.py
+++ Day 8/caesar-cipher.py
@@ -6,8 +6,9 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
-def encode_decode(word, shift, encode=True):
+def encode_decode(word, shift, direction):
     new_word = ''
+    encode = (direction == "encode")
     if not encode:
         shift *= -1
     for i, l in enumerate(word):
@@ -25,10 +26,7 @@ while True:
     shift = int(input("Type the shift number:\n"))
     total = len(alphabet)
 
-    if direction == 'encode':
-        print(encode_decode(word=text, shift=shift))
-    else:
-        print(encode_decode(word=text, shift=shift, encode=False))
+    print(encode_decode(word=text, direction=direction))
 
     con = input("Continue? Y or N: ").lower() == "y"
     if not con:
