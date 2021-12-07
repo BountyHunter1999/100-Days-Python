10f533daa36bf45c5baec07cf2bcd98e52723c3e
mikeyy <adhikari.shrish1@gmail.com>
2021-12-07 20:04:25 +0545

    Basic Operations Calculator done

---
+++ Day 10/calculator.py
@@ -0,0 +1,59 @@
+from logo import logo
+import os
+
+os_clear = 'cls' if os.name == 'nt' else 'clear'
+
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+def calculator():
+    print(logo)
+    num1 = int(input("Enter the first number: "))
+    should_continue = True
+
+    while should_continue:
+        while True:
+            for symbol in operations:
+                print(symbol)
+            operator = input("Choose an operation to perform: ")
+
+            if operator not in operations.keys():
+                print("Choose only the valid operators: \n")
+                continue
+            break
+
+
+        num2 = int(input("Enter the next number: "))
+        result = operations[operator](num1, num2)
+
+        print(f"{num1} {operator} {num2} = {result} ")
+
+        should_continue = input(f"Type 'y' to continue calculating with the answer {result}: ").lower()
+        if should_continue == 'y':
+            num1 = result
+            continue
+        
+        restart = input("\nType 'y' if u want to restart the calculator: ").lower() == 'y'
+        if restart:
+            os.system(os_clear)
+            calculator()
+        
+        return 
+
+calculator()
