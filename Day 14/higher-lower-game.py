eb8a5e0bd409751f7b9ceebe74d797ddf836f23f
mikeyy <adhikari.shrish1@gmail.com>
2021-12-15 15:18:21 +0545

    Added clear screen, sleep and system exit

--- Day 14/higher-lower-game.py
+++ Day 14/higher-lower-game.py
@@ -1,7 +1,12 @@
+import os
+import sys
+from time import sleep
+
 from logo import logo, vs
 from data import data
 from random import choice
 
+os_clear = "cls" if os.name == 'nt' else "clear"
 
 # get two datas
 def get_datas():
@@ -23,16 +28,18 @@ def show_datas(d1, d2, game_on= True):
     """
 
     d2_copy = d2.copy()
+    print("Compare A:")
     for key,value in d1.items():
-        print(f"{key.capitalize()}: {str(value).capitalize()}")
+        print(f"\t {key.capitalize()}: {str(value).capitalize()}")
 
     print(vs)
 
     if game_on:
         del(d2_copy['follower_count'])
 
+    print("Against B:")
     for key,value in d2_copy.items():
-        print(f"{key.capitalize()}: {str(value).capitalize()}")
+        print(f"\t {key.capitalize()}: {str(value).capitalize()}")
 
 def guess_correct(d1, d2, score, guess):
     """
@@ -46,8 +53,10 @@ def guess_correct(d1, d2, score, guess):
     game_on = True
     if d1_score < d2_score and guess == 'h' or d2_score < d1_score and guess == 'l':
         score += 1
+        print("You're Correct!")
     else:
         game_on = False
+        print("You're Wrong!!")
 
     return score, game_on
 
@@ -55,6 +64,9 @@ def game():
     score = 0
     game_on = True
     while game_on:
+        # clean the screen
+        os.system(os_clear)
+
         d1, d2 = get_datas()
         # show_datas(*get_datas())
         show_datas(d1, d2)
@@ -67,6 +79,7 @@ def game():
         score, game_on = guess_correct(d1, d2, score, guess)
         # print(score, game_on)
         print(f"Your score: {score}")
+        sleep(1)
 
     # print the score of the user
     print("The correct data's are:")
@@ -75,6 +88,9 @@ def game():
 
 # ask the user if they want to start the game again 
 while True:
+    print(logo)
     play_game = input("\nPlay Game: \n \t'y': 'yes' \n \t'n': 'no' \n").lower()
     if play_game == 'y':
         game()
+    else:
+        sys.exit()
