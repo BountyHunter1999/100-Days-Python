5aa1a601e1ef5756314916ab4d77bb3b4e723fdc
mikeyy <adhikari.shrish1@gmail.com>
2021-12-20 17:22:52 +0545

    Drawing Polygons of Various Sides

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,14 +1,31 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
 tom = Turtle()
 tom.shape("classic")
-tom.color("blue")
+turtle.colormode(255)
 
-for _ in range(15):
-    tom.forward(10)
-    tom.penup()  # don't leave trail
-    tom.forward(10)
-    tom.pendown()  # Leave trail
+
+def change_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    return r, g, b
+
+
+def draw_shape(sides):
+    internal_angle = int(360 / sides)
+    tom.color(change_color())
+    for _ in range(sides):
+        tom.forward(100)
+        tom.left(internal_angle)
+
+
+for side in range(3, 11):
+    draw_shape(side)
 
 screen = Screen()
+screen.colormode(255)
 screen.exitonclick()
