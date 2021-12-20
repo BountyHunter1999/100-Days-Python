ef7c552d0004cccaf5afb9b363061d7f7dc187e2
mikeyy <adhikari.shrish1@gmail.com>
2021-12-20 16:16:57 +0545

    Dotted Line With Turtle

--- Day 18/main.py
+++ Day 18/main.py
@@ -4,12 +4,11 @@ tom = Turtle()
 tom.shape("classic")
 tom.color("blue")
 
-for _ in range(4):
-    tom.forward(100)
-    tom.left(90)
-
-# tom.forward("100")
-
+for _ in range(15):
+    tom.forward(10)
+    tom.penup()  # don't leave trail
+    tom.forward(10)
+    tom.pendown()  # Leave trail
 
 screen = Screen()
 screen.exitonclick()
