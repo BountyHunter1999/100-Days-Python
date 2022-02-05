f4ff05b0bd41e229bf7839872a01a0280a7dfa09
mikeyy <adhikari.shrish1@gmail.com>
2022-02-05 21:41:57 +0545

    Day 48 Automating with Selenium
    Searching something using search bar(automated)

--- Day 48/task_2/main.py
+++ Day 48/task_2/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "../chromedriver"
 
@@ -10,6 +11,20 @@ URL = "https://en.wikipedia.org/wiki/Main_Page"
 
 driver.get(URL)
 
-print(driver.find_element(by="css selector", value='#articlecount a').text)
+count = driver.find_element(by="css selector", value='#articlecount a')
+print(count.text)
 
-driver.quit()
+# Clicking there
+# count.click()
+
+# as we click on links often we can do something like this too
+all_portals = driver.find_element(by="link text", value="All portals")
+# all_portals.click()
+
+search = driver.find_element(by="name", value="search")
+search.send_keys("Python")
+
+# if we want to send a key that's not a letter or one of the numbers or symbols, line 3
+search.send_keys(Keys.ENTER)
+
+# driver.quit()
